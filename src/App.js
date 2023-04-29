62a5bcf3ebc28f250398ea26a41dd020e33ed787
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-04-29 15:30:56 +0900

    feat: Login 페이지 레이아웃 구현
    - Router 경로 추가
    - useState를 이용하여 id, password 관리
    - form 양식 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
