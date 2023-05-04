75362dc8ac34d38dd24560fbdd1531408faa960d
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-05-04 13:09:58 +0900

    feat: Loading 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Loading from "./components/Loading";
 import Home from "./pages/Home";
 import Keyword from "./pages/Keyword";
 import Login from "./pages/Login";
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/keyword" element={<Keyword />} />
+      <Route path="/loading" element={<Loading />} />
     </Routes>
   );
 }
---
+++ src/components/Loading.js
@@ -0,0 +1,16 @@
+const Loading = ({ text }) => {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
+      <div className="relative w-[120px] h-[120px]">
+        <div className="absolute inset-0 rounded-full bg-blue-500 animate-[loaderAnimate_2s_ease-in-out_infinite]" />
+        <div
+          className="absolute inset-0 rounded-full bg-white bg-opacity-50 backdrop-blur-md animate-[loaderAnimate_2s_ease-in-out_infinite]"
+          style={{ animationDelay: "-1s" }}
+        ></div>
+      </div>
+      <p className="mt-[52px] font-extrabold text-[24px] text-white">{text}</p>
+    </div>
+  );
+};
+
+export default Loading;
