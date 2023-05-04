f5d3b2916a1df297f38485e114b1f4259dda3173
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-05-04 23:09:13 +0900

    feat: TopBar 로그아웃 기능 추가
    - 로그인이 되어 있는 경우, 로그인 버튼 -> 로그아웃 버튼으로 변경
    - 조건부 렌더링을 위하여 기존 map 함수 구조 제거
    - 로그아웃 버튼 클릭 시, recoil의 userState 값을 default 값으로 초기화 및 Home 페이지로 이동

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,30 +1,34 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { userState } from "../atom";
 
 const TopBar = () => {
-  const menus = [
-    {
-      title: "로그인",
-      href: "/login",
-    },
-    {
-      title: "회원가입",
-      href: "/signup",
-    },
-  ];
+  const [user, setUser] = useRecoilState(userState);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setUser({ id: "", isLoggined: false });
+    navigate("/");
+  };
 
   return (
     <header className="fixed top-0 z-50 bg-white w-full h-14 flex items-center justify-center">
       <div className="w-full max-w-6xl flex justify-between p-4 lg:p-6">
         <div />
         <ul className="flex justify-center items-center list-none gap-8">
-          {menus.map((menu) => (
-            <li
-              className="font-pretendard  font-medium text-sm"
-              key={menu.title}
-            >
-              <a href={menu.href}>{menu.title}</a>
-            </li>
-          ))}
+          <li className="font-pretendard font-medium text-sm">
+            {user.isLoggined ? (
+              <p className="cursor-pointer" onClick={() => logout()}>
+                로그아웃
+              </p>
+            ) : (
+              <a href="/login">로그인</a>
+            )}
+          </li>
+          <li className="font-pretendard font-medium text-sm">
+            <a href="/signup">회원가입</a>
+          </li>
         </ul>
       </div>
     </header>
