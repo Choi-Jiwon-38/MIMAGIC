09bd76003877cd483e21757c0fa0c2a0717acd5e
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-05-05 02:33:46 +0900

    chore: firebase cloud function을 위한 프로젝트 설정
    - 기본 설치 시 node 엔진이 16으로 되어 있어 19로 수정 이후 yarn 명령어를 사용하여 기본 설정 완료

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+import * as functions from "firebase-functions";
+import * as express from "express";
+import * as cors from "cors";
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.post("/kakao", async (req, res) => {
+  // TODO: API 구현하기
+});
+
+exports.auth = functions.https.onRequest(app);
