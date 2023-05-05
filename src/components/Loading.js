0fe730b0881e102f2df3b5854a6f8000986f6da8
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-05-06 00:28:52 +0900

    fix: Loading 페이지에 TopBar가 없던 현상 수정

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,21 +1,26 @@
 import { useLocation } from "react-router-dom";
+import TopBar from "./TopBar";
 
 const Loading = ({ text }) => {
   const { state } = useLocation();
 
-  console.log(state);
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
-      <div className="relative w-[120px] h-[120px]">
-        <div className="absolute inset-0 rounded-full bg-blue-500 animate-[loaderAnimate_2s_ease-in-out_infinite]" />
-        <div
-          className="absolute inset-0 rounded-full bg-white bg-opacity-50 backdrop-blur-md animate-[loaderAnimate_2s_ease-in-out_infinite]"
-          style={{ animationDelay: "-1s" }}
-        ></div>
+    <>
+      <TopBar />
+      <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
+        <div className="relative w-[120px] h-[120px]">
+          <div className="absolute inset-0 rounded-full bg-blue-500 animate-[loaderAnimate_2s_ease-in-out_infinite]" />
+          <div
+            className="absolute inset-0 rounded-full bg-white bg-opacity-50 backdrop-blur-md animate-[loaderAnimate_2s_ease-in-out_infinite]"
+            style={{ animationDelay: "-1s" }}
+          ></div>
+        </div>
+        <p className="mt-[52px] font-extrabold text-[24px] text-white">
+          {state ? "Loading for Result . . ." : { text }}
+        </p>
       </div>
-      <p className="mt-[52px] font-extrabold text-[24px] text-white">{ state ? "Loading for Result . . ." :{text} }</p>
-    </div>
+    </>
   );
 };
 
