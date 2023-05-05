f15a9fbf4e2e664018f34e8a06f67c60c9d56699
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-05-06 00:26:22 +0900

    feat: props를 전달 받도록 Loading 페이지 수정

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,6 +1,10 @@
-import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Loading = ({ text }) => {
+  const { state } = useLocation();
+
+  console.log(state);
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center bg-black">
       <div className="relative w-[120px] h-[120px]">
@@ -10,7 +14,7 @@ const Loading = ({ text }) => {
           style={{ animationDelay: "-1s" }}
         ></div>
       </div>
-      <p className="mt-[52px] font-extrabold text-[24px] text-white">{text}</p>
+      <p className="mt-[52px] font-extrabold text-[24px] text-white">{ state ? "Loading for Result . . ." :{text} }</p>
     </div>
   );
 };
